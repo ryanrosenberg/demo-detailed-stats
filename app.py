ee82e9883785cc5eb5b49a037714f9ea55f91680
Ryan Rosenberg <ryan.rosenberg@outlook.com>
2022-05-09 23:31:50 -0700

    add questions tab

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import players
 import teams
 import categories
+import questions
 import streamlit as st
 st.set_page_config(layout="wide")
 
 PAGES = {
     "Players": players,
     "Teams": teams,
-    "Categories": categories
+    "Categories": categories,
+    "Questions": questions
 }
 st.sidebar.title('Navigation')
 selection = st.sidebar.selectbox("Go to", list(PAGES.keys()))
---
+++ questions.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import utils
+from htbuilder import div, p
+import json
+
+def app():
+    st.title('QB League Season 2 Divison 1 -- Questions')
+    st.markdown('<style>#vg-tooltip-element{z-index: 1000051}</style>',
+                unsafe_allow_html=True)
+    st.markdown('''<style>
+    .buzz {display: inline; background-color: #e4e1e2;}
+    .buzz-value {display: inline; background-color: #e4e1e2; font-size: 80%; color: #555555;}
+    p {display: inline;}
+    </style>''',
+                unsafe_allow_html=True)
+
+    buzzes, bonuses, tossup_meta = utils.load_data()
+    full_buzzes = buzzes.merge(tossup_meta[tossup_meta['season'] == 2], on=['packet', 'tossup'])
+
+    pac = st.selectbox('Packet', options = range(5, 7))
+    tu = st.selectbox('Tossup', options = range(1, 21), format_func= lambda x: str(x) + ' (' + full_buzzes['answer'][full_buzzes['packet'] == pac].unique()[x-1] + ')')
+
+    with open(f'packet{pac}.json', 'r') as f:
+        question_text = json.load(f)
+
+    sani = question_text['tossups'][tu - 1]['question'].split(' ')
+    qbuzz = buzzes[buzzes['packet'] == pac][buzzes['tossup'] == tu]
+    for i, row in qbuzz.iterrows():
+        sani[row['buzz_position']] = str(
+            div(_class = 'buzz')(
+                sani[row['buzz_position']],
+                p(_class = 'buzz-value')(' ' + str(row['buzz_value']))
+                )
+        )
+
+    sani = ' '.join(sani)
+
+    st.markdown(sani,
+    unsafe_allow_html=True)
+    st.markdown('ANSWER: ' + question_text['tossups'][tu - 1]['answer'],
+    unsafe_allow_html=True)
+
+    qbuzz['packet'] = qbuzz['packet'].astype(int)
+    st.dataframe(qbuzz.sort_values('buzz_position', ascending=True))
+    
+
+    
