6f45c874b9893376b28ac534f509dcd180f959de
Ryan Rosenberg <ryan.rosenberg@outlook.com>
2022-05-09 21:44:36 -0700

    add other tabs

---
+++ players.py
@@ -0,0 +1,95 @@
+import streamlit as st
+import utils
+
+def app():
+    st.title('QB League Season 2 Divison 1 -- Players')
+    st.markdown('<style>#vg-tooltip-element{z-index: 1000051}</style>',
+                unsafe_allow_html=True)
+    # CSS to inject contained in a string
+    hide_table_row_index = """
+                <style>
+                .row_heading.level0 {display:none}
+                .stDataFrame {border:1px solid white}
+                .blank {display:none}
+                </style>
+                """
+
+    # Inject CSS with Markdown
+    st.markdown(hide_table_row_index, unsafe_allow_html=True)
+
+    buzzes, bonuses, tossup_meta = utils.load_data()
+    full_buzzes = buzzes.merge(tossup_meta[tossup_meta['season'] == 2], on=['packet', 'tossup'])
+    player_summary = buzzes.groupby(
+            ['player', 'team', 'buzz_value']
+            ).agg(
+                'size'
+                ).reset_index().pivot(
+                    index = ['player', 'team'], columns='buzz_value', values=0
+                    ).reset_index().rename(columns={15: 'P', 10: 'G', -5:'N'})
+
+    player_cat_ranks = full_buzzes.groupby(['player', 'team', 'category', 'buzz_value']).agg(
+                'size'
+                ).reset_index().pivot(
+                    index = ['player', 'team', 'category'], columns='buzz_value', values=0
+                    ).reset_index().rename(columns={15: 'P', 10: 'G', -5:'N'})
+
+    player_summary = utils.fill_out_tossup_values(player_summary)
+    player_cat_ranks = utils.fill_out_tossup_values(player_cat_ranks)
+
+    player_summary = player_summary[['player', 'team', 'P', 'G', 'N']].fillna(0).assign(
+                        Pts = lambda x: x.P*15 + x.G*10 - x.N*5
+                        ).sort_values(['Pts'], ascending=False)
+
+    player_cat_ranks = player_cat_ranks[['player', 'team', 'category', 'P', 'G', 'N']].fillna(0).assign(
+                        Pts = lambda x: x.P*15 + x.G*10 - x.N*5
+                        )
+    player_cat_ranks['rank'] = player_cat_ranks.groupby('category')['Pts'].rank('min', ascending=False)
+
+
+    st.write("Click on a player's row to show more information!")
+    selection = utils.aggrid_interactive_table(player_summary)
+    
+    if selection["selected_rows"]:
+        st.subheader(f"{selection['selected_rows'][0]['player']}, {selection['selected_rows'][0]['team']}")
+        player_buzzes = full_buzzes[full_buzzes['player'] == selection["selected_rows"][0]['player']][full_buzzes['team'] == selection["selected_rows"][0]['team']]
+        player_cats = player_buzzes.groupby(
+            ['player', 'team', 'category', 'buzz_value']
+            ).agg('size').reset_index().pivot(
+            index = ['player', 'team', 'category'], columns='buzz_value', values=0
+            ).reset_index().rename(columns={15: 'P', 10: 'G', -5:'N'})
+
+        player_cats = utils.fill_out_tossup_values(player_cats)
+                
+        player_cats = player_cats[['player', 'team', 'category', 'P', 'G', 'N']].fillna(0).assign(Pts = lambda x: x.P*15 + x.G*10 - x.N*5)
+        player_cats[['P', 'G', 'N', 'Pts']] = player_cats[['P', 'G', 'N', 'Pts']].astype(int).sort_values(['Pts'], ascending=False)
+
+        st.subheader('Categories')
+        col1, col2 = st.columns(2)
+        player_rank = player_cats.merge(player_cat_ranks[['player', 'team', 'category', 'rank']], on=['player', 'team', 'category'])
+        player_rank = player_rank[['category', 'P', 'G', 'N', 'Pts', 'rank']].rename(columns={'rank': 'Rk'})
+        player_rank['Rk'] = player_rank['Rk'].astype(int)
+        col1.dataframe(player_rank)
+        if max(player_cats[['P', 'G', 'N']].max()) >= 2:
+            col2.pyplot(utils.make_category_buzz_chart(player_buzzes))
+        
+        player_subcats = player_buzzes.groupby(['category', 'subcategory', 'buzz_value']).agg(
+        'size'
+        ).reset_index().pivot(
+            index = ['category', 'subcategory'], columns='buzz_value', values=0
+            ).reset_index().rename(columns={15: 'P', 10: 'G', -5:'N'})
+
+        player_subcats = utils.fill_out_tossup_values(player_subcats)
+
+        player_subcats = player_subcats[['category', 'subcategory', 'P', 'G', 'N']].fillna(0).assign(Pts = lambda x: x.P*15 + x.G*10 - x.N*5)
+        player_subcats[['P', 'G', 'N', 'Pts']] = player_subcats[['P', 'G', 'N', 'Pts']].astype(int).sort_values(['Pts'], ascending=False)
+
+        st.subheader('Subcategories')
+        col3, col4 = st.columns(2)
+        col3.dataframe(player_subcats)
+        # col4.pyplot(utils.make_subcategory_buzz_chart(player_buzzes))
+
+        st.subheader('Buzzes')
+        player_buzzes['packet'] = player_buzzes['packet'].astype(int)
+        st.dataframe(player_buzzes[['packet', 'tossup', 'category', 'subcategory', 'answer', 'buzz_position', 'buzz_value']])
+
+
---
+++ teams.py
@@ -0,0 +1,132 @@
+import streamlit as st
+import utils
+
+def app():
+    st.title('QB League Season 2 Divison 1 -- Teams')
+    st.markdown('<style>#vg-tooltip-element{z-index: 1000051}</style>',
+                unsafe_allow_html=True)
+    # CSS to inject contained in a string
+    hide_table_row_index = """
+                <style>
+                .row_heading.level0 {display:none}
+                .stDataFrame {border:1px solid white}
+                .blank {display:none}
+                </style>
+                """
+
+    # Inject CSS with Markdown
+    st.markdown(hide_table_row_index, unsafe_allow_html=True)
+
+    buzzes, bonuses, tossup_meta = utils.load_data()
+    full_buzzes = buzzes.merge(tossup_meta[tossup_meta['season'] == 2], on=['packet', 'tossup'])
+    team_summary = buzzes.groupby(
+            ['team', 'buzz_value']
+            ).agg(
+                'size'
+                ).reset_index().pivot(
+                    index = ['team'], columns='buzz_value', values=0
+                    ).reset_index().rename(columns={15: 'P', 10: 'G', -5:'N'})
+
+    team_cat_ranks = full_buzzes.groupby(['team', 'category', 'buzz_value']).agg(
+                'size'
+                ).reset_index().pivot(
+                    index = ['team', 'category'], columns='buzz_value', values=0
+                    ).reset_index().rename(columns={15: 'P', 10: 'G', -5:'N'})
+
+    team_summary = utils.fill_out_tossup_values(team_summary)
+    team_cat_ranks = utils.fill_out_tossup_values(team_cat_ranks)
+
+    team_summary = team_summary[['team', 'P', 'G', 'N']].fillna(0).assign(
+                        Pts = lambda x: x.P*15 + x.G*10 - x.N*5
+                        ).sort_values(['Pts'], ascending=False)
+
+    team_cat_ranks = team_cat_ranks[['team', 'category', 'P', 'G', 'N']].fillna(0).assign(
+                        Pts = lambda x: x.P*15 + x.G*10 - x.N*5
+                        )
+    team_cat_ranks['rank'] = team_cat_ranks.groupby('category')['Pts'].rank('min', ascending=False)
+
+    st.header('Tossup data')
+    st.write("Click on a team's row to show more information!")
+    selection = utils.aggrid_interactive_table(team_summary)
+    
+    if selection["selected_rows"]:
+        st.subheader(selection['selected_rows'][0]['team'])
+        team_buzzes = full_buzzes[full_buzzes['team'] == selection["selected_rows"][0]['team']]
+        
+        st.subheader('Players')
+        player_stats = team_buzzes.groupby(
+            ['player', 'buzz_value']
+            ).agg('size').reset_index().pivot(
+            index = ['player'], columns='buzz_value', values=0
+            ).reset_index().rename(columns={15: 'P', 10: 'G', -5:'N'})
+
+        player_stats = utils.fill_out_tossup_values(player_stats).fillna(0).assign(Pts = lambda x: x.P*15 + x.G*10 - x.N*5)[['player', 'P', 'G', 'N', 'Pts']]
+        player_stats[['P', 'G', 'N', 'Pts']] = player_stats[['P', 'G', 'N', 'Pts']].astype(int)
+        st.dataframe(player_stats.sort_values('Pts', ascending=False))
+        
+        team_cats = team_buzzes.groupby(
+            ['team', 'category', 'buzz_value']
+            ).agg('size').reset_index().pivot(
+            index = ['team', 'category'], columns='buzz_value', values=0
+            ).reset_index().rename(columns={15: 'P', 10: 'G', -5:'N'})
+
+        team_cats = utils.fill_out_tossup_values(team_cats)
+                
+        team_cats = team_cats[['team', 'category', 'P', 'G', 'N']].fillna(0).assign(Pts = lambda x: x.P*15 + x.G*10 - x.N*5)
+        team_cats[['P', 'G', 'N', 'Pts']] = team_cats[['P', 'G', 'N', 'Pts']].astype(int).sort_values(['Pts'], ascending=False)
+
+        st.subheader('Categories')
+        col1, col2 = st.columns(2)
+        team_rank = team_cats.merge(team_cat_ranks[['team', 'category', 'rank']], on=['team', 'category'])
+        team_rank = team_rank[['category', 'P', 'G', 'N', 'Pts', 'rank']].rename(columns={'rank': 'Rk'})
+        team_rank['Rk'] = team_rank['Rk'].astype(int)
+        col1.dataframe(team_rank)
+        if max(team_cats[['P', 'G', 'N']].max()) >= 2:
+            col2.pyplot(utils.make_category_buzz_chart(team_buzzes))
+        
+        team_subcats = team_buzzes.groupby(['category', 'subcategory', 'buzz_value']).agg(
+        'size'
+        ).reset_index().pivot(
+            index = ['category', 'subcategory'], columns='buzz_value', values=0
+            ).reset_index().rename(columns={15: 'P', 10: 'G', -5:'N'})
+
+        team_subcats = utils.fill_out_tossup_values(team_subcats)
+
+        team_subcats = team_subcats[['category', 'subcategory', 'P', 'G', 'N']].fillna(0).assign(Pts = lambda x: x.P*15 + x.G*10 - x.N*5)
+        team_subcats[['P', 'G', 'N', 'Pts']] = team_subcats[['P', 'G', 'N', 'Pts']].astype(int).sort_values(['Pts'], ascending=False)
+
+        st.subheader('Subcategories')
+        col3, col4 = st.columns(2)
+        col3.dataframe(team_subcats)
+        # col4.pyplot(utils.make_subcategory_buzz_chart(player_buzzes))
+
+        st.subheader('Buzzes')
+        team_buzzes['packet'] = team_buzzes['packet'].astype(int)
+        st.dataframe(team_buzzes[['packet', 'tossup', 'category', 'subcategory', 'answer', 'buzz_position', 'buzz_value']])
+
+
+
+    bonus_cat = st.container()
+
+    bonus_summary = bonuses.assign(
+        tot = lambda x: x.part1_value + x.part2_value + x.part3_value
+        ).groupby('team').agg({'tot': 'mean'}).reset_index().rename(
+            columns={'tot': 'PPB'}
+            ).sort_values('PPB', ascending=False)
+
+    bonus_summary['PPB'] = round(bonus_summary['PPB'], 2)
+
+    with bonus_cat:
+        st.header('Bonus data')
+        col3, col4 = st.columns(2)
+        with col3:
+            selection = utils.aggrid_interactive_table(bonus_summary)
+
+        with col4:
+            if selection["selected_rows"]:
+                team_bonuses = bonuses[bonuses['team'] == selection["selected_rows"][0]['team']]
+                st.table(team_bonuses.assign(
+        tot = lambda x: x.part1_value + x.part2_value + x.part3_value
+        ).groupby('packet').agg({'tot': 'mean'}).reset_index().rename(
+            columns={'tot': 'PPB'}
+            ))
