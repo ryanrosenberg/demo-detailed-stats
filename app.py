edbf363b8947412a0684bdacbd41265fd23e639e
Ryan Rosenberg <ryan.rosenberg@outlook.com>
2022-05-12 04:32:03 -0700

    change sidebar to menu

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import bonuses
 import games
 import streamlit as st
 st.set_page_config(layout="wide")
+from streamlit_option_menu import option_menu
 
 PAGES = {
     "Players": players,
@@ -17,8 +18,17 @@ PAGES = {
     "Bonuses": bonuses,
     "Games" : games
 }
-st.sidebar.title('Navigation')
-selection = st.sidebar.selectbox("Go to", list(PAGES.keys()))
+with st.sidebar:
+    selection = option_menu("Go to", list(PAGES.keys()),
+    menu_icon="arrow-down-right",
+    styles={
+        "menu-title": {"font-size": "20px", "text-align": "left", "margin":"0px"},
+        "container": {"padding": "0!important", "background-color": "#F0F2F6"},
+        "icon": {"color": "black", "font-size": "16px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+    })
+
+st.sidebar.subheader('Filters')
 tournaments = st.sidebar.multiselect("Divisions", ['01', '2a', '2b', '3a', '3b'], format_func=lambda x: '1' if x == '01' else x)
 
 page = PAGES[selection]
